8280f8000c8716ccd69000ed5a9616d373618916
StepanZyznikov <74812822+StepanZyznikov@users.noreply.github.com>
2025-09-12 19:00:14 -0400

    Update Fractals_colored.py
    Added classes and fractal variations

--- Fractals_colored.py
+++ Fractals_colored.py
@@ -11,87 +11,96 @@ import matplotlib as mpl
 import numpy as np
 import copy
 
-
 ## Classes
 class Coordinates:
     def __init__(self, x, y):
         self.x = x 
         self.y = y
 
-
 ## Constants
 CANVAS_SIZE = 1
 CANVAS_SIZE_INCH = 7
-VERTEX_COLOR = '#FF0000'
-START_COLOR = "#00FF00"
-DOT_COLOR = '#000000'
 DOT_NUMBER = 10**5
-
-
-## Variable Names
-vertex_number = 3 # Number of vertexies of the fractal
-jump_distance = 1 # The jump distance
-travel_distance = 2 # The travel distance
-
 
 ## Functions
 def wrapped_input(data_type=str, input_message=''):
     return data_type(input(input_message))
 
-def travel_dist_calc(dot1: Coordinates, dot2: Coordinates, travel_dist_coef):
+def kissing_ratio(vertex_number): # https://www.sciencedirect.com/science/article/pii/S096007792100494X
+    a = np.pi/vertex_number
+    e = 2 * a * (np.ceil(vertex_number/4) - 1 / 2) - np.pi / 2
+    return round(1 - np.sin(a) / (np.sin(a) + np.cos(e)), 3)
+
+def travel_performer(dot1: Coordinates, dot2: Coordinates, travel_dist_coef):
     return Coordinates(dot1.x * (1-travel_dist_coef) + dot2.x * travel_dist_coef,
                        dot1.y * (1-travel_dist_coef) + dot2.y * travel_dist_coef)
 
-def dot_generation(vertex_coords, jump_distance, vertex_number, travel_distance=0.5, start_coords=[0,0]):
-    rng = np.random.default_rng()
-    prev_coords = start_coords
-    curr_vertex_index = rng.integers(vertex_number)
+def dot_generation(vertex_coords, mode):
+    vertex_number = len(vertex_coords)
+
+    if mode == 1 or mode == 2 and vertex_number == 4:
+        travel_distance = 0.5
+        jump_distance = 1
+    elif mode == 3:
+        jump_distance = vertex_number // 2 if vertex_number != 4 else 1
+        travel_distance = 2
+        mode = 0
+    else:
+        travel_distance = kissing_ratio(vertex_number)
+        jump_distance = vertex_number // 2 if vertex_number != 4 else 1
+        mode = 0
+    
+    prev_coords = Coordinates(0, 0)
     dot_coords = []
+
+    rng = np.random.default_rng()
+    curr_vertex_index = rng.integers(vertex_number)
+
     for i in range(vertex_number):
-        dot_coords.append(copy.deepcopy([[], '#121212']))
+        dot_coords.append(copy.deepcopy([[]]))
+
     for i in range(DOT_NUMBER):
-        shift = rng.integers(2*jump_distance+1)-jump_distance
-        curr_vertex_index = (shift + curr_vertex_index + vertex_number) % vertex_number
-        dot_coords[curr_vertex_index][0].append([(prev_coords[0] + vertex_coords[curr_vertex_index][0])*travel_distance, 
-                                                    (prev_coords[1] + vertex_coords[curr_vertex_index][1])*travel_distance])
+        if 2 * jump_distance + 1 > vertex_number:
+            shift = rng.integers(vertex_number)
+        else:
+            shift = rng.integers(2*jump_distance+1)-jump_distance
+        curr_vertex_index = (shift + curr_vertex_index + vertex_number + mode) % vertex_number
+        dot_coords[curr_vertex_index][0].append(travel_performer(prev_coords, vertex_coords[curr_vertex_index], travel_distance))
         prev_coords = dot_coords[curr_vertex_index][0][-1]
+        
     return dot_coords
 
 
 ## Inputs
 vertex_number = wrapped_input(int, "Enter integer vertex number:")
-jump_distance = wrapped_input(int, "Enter integer jump distance:")
-travel_distance = wrapped_input(float, "Enter divisor for travel distance:")
-if travel_distance <= 0:
-    travel_distance = 1/2
+mode = wrapped_input(int, "Do you want to generate a regular fractal? 0 for yes.")
+if mode == 3: # Quazi-stable fractal generation
+    CANVAS_SIZE = 1000
 
 
 # plot
 plt.style.use('_mpl-gallery')
 fig = plt.figure(figsize=(CANVAS_SIZE_INCH, CANVAS_SIZE_INCH)) 
 ax = plt.subplot()
-ax.set(xlim=(-CANVAS_SIZE, CANVAS_SIZE), xticks=np.arange(-CANVAS_SIZE, CANVAS_SIZE), 
-       ylim=(-CANVAS_SIZE, CANVAS_SIZE), yticks=np.arange(-CANVAS_SIZE, CANVAS_SIZE))
-
-cmap = mpl.colormaps["plasma"]
-VERTEX_COLORS = [cmap(i/vertex_number) for i in range(vertex_number)]
+ax.set(xlim=(-CANVAS_SIZE, CANVAS_SIZE), xticks=[], ylim=(-CANVAS_SIZE, CANVAS_SIZE), yticks=[])
 
 
 # vertex initialization
-vertex_coords = [[np.cos(2*np.pi*i/vertex_number), np.sin(2*np.pi*i/vertex_number)] for i in range(1, vertex_number + 1)]
+cmap = mpl.colormaps["plasma"]
+vertex_colors = [cmap(i/vertex_number) for i in range(vertex_number)]
+
+if vertex_number == 4:
+    vertex_coords= [Coordinates(1, 1), Coordinates(-1, 1), Coordinates(-1, -1), Coordinates(1, -1)]
+else:
+    vertex_coords = [Coordinates(np.cos(2*np.pi*i/vertex_number), np.sin(2*np.pi*i/vertex_number)) for i in range(1, vertex_number + 1)]
 
 
 # starting position generation
-start_coords = Coordinates[0, 0]
-dot_coords = dot_generation(vertex_coords, jump_distance, vertex_number, travel_distance, start_coords)
+dot_coords = dot_generation(vertex_coords, mode)
 
 
 # plotting
-ax.scatter(start_coords[0], start_coords[1], s=10, c=START_COLOR)
 for i in range(vertex_number):
-    ax.scatter([j[0] for j in dot_coords[i][0]], [j[1] for j in dot_coords[i][0]], s=10**3/DOT_NUMBER, c=VERTEX_COLORS[i])
-
-for i in range(vertex_number):
-    ax.scatter(vertex_coords[i][0], vertex_coords[i][1], s=50, c=VERTEX_COLORS[i])
+    ax.scatter([j.x for j in dot_coords[i][0]], [j.y for j in dot_coords[i][0]], s=10**3/DOT_NUMBER, c=vertex_colors[i])
 
 plt.show()
